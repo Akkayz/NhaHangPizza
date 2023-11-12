4f4d6971ca1189ea053e2d21cc45e8d59d778348
Akkay <nguyenhung.dev.info@gmail.com>
2023-11-13 02:47:15 +0700

    - Add  LayoutTrangChu - Add BanhPizzaPartial

--- NhaHangPIzza/Controllers/HomeController.cs
+++ NhaHangPIzza/Controllers/HomeController.cs
@@ -13,18 +13,9 @@ namespace NhaHangPIzza.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult BanhPizzaPartial()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return PartialView();
         }
     }
 }
