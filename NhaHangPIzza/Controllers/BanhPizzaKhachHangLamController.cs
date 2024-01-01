2152b0a0285c9367ab0e5a98f43b00d6553ebf1b
Akkay <nguyenhung.dev.info@gmail.com>
2024-01-02 02:49:44 +0700

    Hiển thị bánh khách hàng làm theo bàn

--- NhaHangPIzza/Controllers/BanhPizzaKhachHangLamController.cs
+++ NhaHangPIzza/Controllers/BanhPizzaKhachHangLamController.cs
@@ -9,25 +9,40 @@ namespace NhaHangPIzza.Controllers
     {
         private readonly QLNHAHANG_PIZZAEntities db = new QLNHAHANG_PIZZAEntities();
 
-        public ActionResult Index()
+        public ActionResult Index(int? selectedBan)
         {
             // Truy vấn bánh pizza khách hàng làm
             var danhSachBanhPizza = db.MonAns
                 .Where(m => m.TheLoai == false) // Lọc theo TheLoai = false
                 .ToList();
 
-            // Truy vấn các hóa đơn đã thanh toán
-            var hoaDonDaThanhToan = db.HoaDons
-                .Where(hd => hd.TrangThai == "Đã thanh toán")
-                .SelectMany(hd => hd.ChiTietMonAn_HoaDon.Select(ct => ct.MaMonAn))
-                .ToList();
+            // Kiểm tra nếu có mã bàn được chọn, thêm điều kiện lọc theo mã bàn
+            if (selectedBan.HasValue)
+            {
+                danhSachBanhPizza = danhSachBanhPizza
+                    .Where(m => m.Maban == selectedBan.Value)
+                    .ToList();
+            }
+            else
+            {
+                // Truy vấn các hóa đơn đã thanh toán
+                var hoaDonDaThanhToan = db.HoaDons
+                    .Where(hd => hd.TrangThai == "Đã thanh toán")
+                    .SelectMany(hd => hd.ChiTietMonAn_HoaDon.Select(ct => ct.MaMonAn))
+                    .ToList();
 
-            // Loại bỏ bánh pizza có mã nằm trong ChiTietMonAn_HoaDon của hóa đơn đã thanh toán
-            danhSachBanhPizza = danhSachBanhPizza.Where(m => !hoaDonDaThanhToan.Contains(m.MaMonAn)).ToList();
+                // Loại bỏ bánh pizza có mã nằm trong ChiTietMonAn_HoaDon của hóa đơn đã thanh toán
+                danhSachBanhPizza = danhSachBanhPizza.Where(m => !hoaDonDaThanhToan.Contains(m.MaMonAn)).ToList();
+            }
+
             List<BAN> danhSachBan = db.BANs.ToList();
             ViewBag.DanhSachBan = danhSachBan;
             ViewBag.DanhSachKichThuoc = db.KichThuocBanhs.ToList();
             ViewBag.DanhSachVoBanh = db.VOBANHs.ToList();
+
+            // Truyền mã bàn đã chọn vào ViewBag
+            ViewBag.SelectedBan = selectedBan;
+
             return View(danhSachBanhPizza);
         }
     }
